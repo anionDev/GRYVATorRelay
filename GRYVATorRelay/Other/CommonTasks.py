ed3b9a9d8d583fa105db994e4937cdc0c7d84538
Marius Göcke <marius.goecke@gmail.com>
2022-12-03 11:13:06 +0100

    update for structure

---
+++ GRYVATorRelay/Other/CommonTasks.py
@@ -0,0 +1,14 @@
+import sys
+import os
+from pathlib import Path
+from ScriptCollection.GeneralUtilities import GeneralUtilities
+from ScriptCollection.ScriptCollectionCore import ScriptCollectionCore
+from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjectStructure
+
+
+def common_tasks():
+    pass  # TODO
+
+
+if __name__ == "__main__":
+    common_tasks()
---
+++ GRYVATorRelay/Other/QualityCheck/RunTestcases.py
@@ -0,0 +1,11 @@
+import sys
+from pathlib import Path
+from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjectStructure
+
+
+def run_testcases():
+    pass  # TODO
+
+
+if __name__ == "__main__":
+    run_testcases()
