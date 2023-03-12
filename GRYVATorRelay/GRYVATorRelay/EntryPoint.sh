1361f38b71570b5da66ffd5c4b5dac5909b5fcff
Marius Göcke <marius.goecke@gmail.com>
2023-03-12 20:05:45 +0100

    readded entrypoint

---
+++ GRYVATorRelay/GRYVATorRelay/EntryPoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+sed -i -e "s/__.torrc.nickname.__/$nickname/g" ./torrc
+sed -i -e "s/__.torrc.orport.__/$orport/g" ./torrc
+sed -i -e "s/__.torrc.exitrelay.__/$exitrelay/g" ./torrc
+sed -i -e "s/__.torrc.socksport.__/$socksport/g" ./torrc
+sed -i -e "s/__.torrc.controlsocket.__/$controlsocket/g" ./torrc
+sed -i -e "s/__.torrc.contactinfo.__/$contactinfo/g" ./torrc
+sed -i -e "s/__.torrc.relaybandwidthrate.__/$relaybandwidthrate/g" ./torrc
+
+if [[ "${lognotice}" = true ]]; then
+  sed -i -e "s/__.torrc.lognotice.__/Log notice file \/var\/log\/tor\/notices.log/g" ./torrc
+else
+  sed -i -e "s/__.torrc.lognotice.__//g" ./torrc
+fi
+if [[ "${logdebug}" = true ]]; then
+  sed -i -e "s/__.torrc.logdebug.__/Log notice file \/var\/log\/tor\/debug.log/g" ./torrc
+else
+  sed -i -e "s/__.torrc.logdebug.__//g" ./torrc
+fi
+
+sed -i '/^$/d' ./torrc # Remove empty lines
+sed -i '' -e '$a\' ./torrc # Append empty line
+
+echo "--------------------"
+echo "Tor-version:"
+tor --version
+echo "--------------------"
+echo "Tor-Configuration:"
+cat ./torrc
+echo "--------------------"
+echo "Starting tor"
+tor -f ./torrc
