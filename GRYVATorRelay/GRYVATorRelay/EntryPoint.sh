134c920c9f3473acd01583e02fb3817240de1e26
Marius Göcke <marius.goecke@gmail.com>
2023-03-13 21:25:21 +0100

    fix

--- GRYVATorRelay/GRYVATorRelay/EntryPoint.sh
+++ GRYVATorRelay/GRYVATorRelay/EntryPoint.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
 configurationfile="/Workspace/Application/torrc"
-sed -i -e "s/__.torrc.nickname.__/$nickname/g" "$configurationfile"
-sed -i -e "s/__.torrc.orport.__/$orport/g" "$configurationfile"
-sed -i -e "s/__.torrc.exitrelay.__/$exitrelay/g" "$configurationfile"
-sed -i -e "s/__.torrc.socksport.__/$socksport/g" "$configurationfile"
-sed -i -e "s/__.torrc.controlsocket.__/$controlsocket/g" "$configurationfile"
-sed -i -e "s/__.torrc.contactinfo.__/$contactinfo/g" "$configurationfile"
-sed -i -e "s/__.torrc.relaybandwidthrate.__/$relaybandwidthrate/g" "$configurationfile"
+
+sed -i -e "s/__.torrc.nickname.__/$nickname/g" $configurationfile
+sed -i -e "s/__.torrc.orport.__/$orport/g" $configurationfile
+sed -i -e "s/__.torrc.exitrelay.__/$exitrelay/g" $configurationfile
+sed -i -e "s/__.torrc.socksport.__/$socksport/g" $configurationfile
+sed -i -e "s/__.torrc.controlsocket.__/$controlsocket/g" $configurationfile
+sed -i -e "s/__.torrc.contactinfo.__/$contactinfo/g" $configurationfile
+sed -i -e "s/__.torrc.relaybandwidthrate.__/$relaybandwidthrate/g" $configurationfile
 
 if [[ "${lognotice}" = true ]]; then
-  sed -i -e "s/__.torrc.lognotice.__/Log notice file \/var\/log\/tor\/notices.log/g" "$configurationfile"
+  sed -i -e "s/__.torrc.lognotice.__/Log notice file \/var\/log\/tor\/notices.log/g" $configurationfile
 else
-  sed -i -e "s/__.torrc.lognotice.__//g" "$configurationfile"
+  sed -i -e "s/__.torrc.lognotice.__//g" $configurationfile
 fi
 if [[ "${logdebug}" = true ]]; then
-  sed -i -e "s/__.torrc.logdebug.__/Log notice file \/var\/log\/tor\/debug.log/g" "$configurationfile"
+  sed -i -e "s/__.torrc.logdebug.__/Log notice file \/var\/log\/tor\/debug.log/g" $configurationfile
 else
-  sed -i -e "s/__.torrc.logdebug.__//g" "$configurationfile"
+  sed -i -e "s/__.torrc.logdebug.__//g" $configurationfile
 fi
 
-sed -i '/^$/d' "$configurationfile" # Remove empty lines
-sed -i '' -e '$a\' "$configurationfile" # Append empty line
+# Remove empty lines
+sed -i '/^$/d' $configurationfile
+# Append empty line
+sed -i '' -e '$a\' $configurationfile
 
 echo "--------------------"
 echo "Tor-version:"
 tor --version
 echo "--------------------"
 echo "Tor-Configuration:"
-cat "$configurationfile"
+cat $configurationfile
 echo "--------------------"
 echo "Starting tor..."
-tor -f "$configurationfile"
+tor -f $configurationfile
